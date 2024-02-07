ea3be3d17795523d236892579fac373f240f2d38
palwashasheikh <palwashasheikh229@gmail.com>
2024-02-07 17:31:40 +0400

    db update

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const Connection = async (username, password) => {
     // const URL = `mongodb://${username}:${password}@ecommerceweb-shard-00-00.fdvft.mongodb.net:27017,ecommerceweb-shard-00-01.fdvft.mongodb.net:27017,ecommerceweb-shard-00-02.fdvft.mongodb.net:27017/ECOMMERCE?ssl=true&replicaSet=atlas-8a6bhp-shard-0&authSource=admin&retryWrites=true&w=majority`;
     // const URL = `mongodb://${username}:${password}@ecommerce-shard-00-00.fdvft.mongodb.net:27017,ecommerce-shard-00-01.fdvft.mongodb.net:27017,ecommerce-shard-00-02.fdvft.mongodb.net:27017/ECOMMERCE?ssl=true&replicaSet=atlas-ilaj5d-shard-0&authSource=admin&retryWrites=true&w=majority`;
-    const URL = `mongodb+srv://user:Izhaan123@cluster0.uzgbews.mongodb.net/?retryWrites=true&w=majority`
+    const URL = `mongodb+srv://user:123@cluster0.ipeb6cq.mongodb.net/?retryWrites=true&w=majority`
     try {
         await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
         console.log('Database Connected Succesfully');
